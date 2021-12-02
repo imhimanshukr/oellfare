e0701b012e986827693f102ac473f909052f04df
Himanshu Kumar <63518367+imhimanshukr@users.noreply.github.com>
2021-12-02 14:49:30 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+// Nav Hide on click
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+let navToggle = document.querySelector('.navbar-toggler')
+navBar.forEach(function(e){
+    e.addEventListener('click', function(){
+        navCollapse.classList.remove('show');
+        navToggle.classList.remove('opened')
+    })
+})
+let DropdownNavBar = document.querySelectorAll('.dropdown-item');
+let DropdownNavCollapse = document.querySelector('.navbar-collapse.collapse');
+
+DropdownNavBar.forEach(function(e){
+    e.addEventListener('click', function(){
+        DropdownNavCollapse.classList.remove('show');
+    })
+})
+
 // Progress Bar
 function increase() {
     let Speed = 40;
@@ -11,4 +30,12 @@ function increase() {
 }
 increase();
 
-// Animated Text
+// On scroll Navbar
+let nav = document.querySelector('.navigation-wrap');
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add('scroll-on')
+    }else{
+        nav.classList.remove('scroll-on')
+    }
+}
